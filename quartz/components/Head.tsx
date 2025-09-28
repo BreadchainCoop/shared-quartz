a0e588dc84c06e81b83609f6bb70e0f3f7af61ac
rathermercurial <rathermercurial@protonmail.com>
2025-09-28 13:13:58 -0700

    Fix font preload MIME type in Head.tsx
    Change from invalid 'font/otf' to standard 'font' type for maximum browser compatibility.

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -53,10 +53,10 @@ export default (() => {
         )}
         {cfg.theme.fontOrigin === "local" && (
           <>
-            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-body-regular.otf")} as="font" type="font/otf" crossOrigin="anonymous" />
-            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-display-regular.otf")} as="font" type="font/otf" crossOrigin="anonymous" />
-            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-body-bold.otf")} as="font" type="font/otf" crossOrigin="anonymous" />
-            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-display-black.otf")} as="font" type="font/otf" crossOrigin="anonymous" />
+            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-body-regular.otf")} as="font" type="font" crossOrigin="anonymous" />
+            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-display-regular.otf")} as="font" type="font" crossOrigin="anonymous" />
+            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-body-bold.otf")} as="font" type="font" crossOrigin="anonymous" />
+            <link rel="preload" href={joinSegments(baseDir, "static/fonts/bread-display-black.otf")} as="font" type="font" crossOrigin="anonymous" />
           </>
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
